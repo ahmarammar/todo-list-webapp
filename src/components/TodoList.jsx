4c4e26297c9f8bc4c53d3a6b894068e32dd28788
Ahmar Ammar <211370160@gift.edu.pk>
2023-03-23 08:24:10 +0500

    Animation added for the create and delete operations

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Todo from './Todo'
+import { useAutoAnimate } from '@formkit/auto-animate/react'
 
 const TodoList = ({ todos, filterCheck, filterDelete }) => {
   
+  const [animateTodo] = useAutoAnimate();
+
   const todoListCheck = ( id ) => {
     filterCheck(id);
   }
@@ -15,7 +18,7 @@ const TodoList = ({ todos, filterCheck, filterDelete }) => {
     <>
       <div className='bg-veryDarkSaturatedBlue rounded-lg'>
         <div>
-          <ul className='flex flex-col'>
+          <ul className='flex flex-col' ref={animateTodo}>
             {todos.map((todo,index) => {
               return <Todo key={index} completed={todo.completed} id={todo.id} text={todo.text} todoListCheck = {todoListCheck} todoListDelete = {todoListDelete} />
             })}
