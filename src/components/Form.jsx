a7834430586129dc935149341adb9f05b517c888
Ahmar Ammar <211370160@gift.edu.pk>
2023-05-05 11:12:29 +0500

    Fixed responsiveness for the smaller screens devices

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -20,9 +20,9 @@ const Form = ({ handleTodos }) => {
   return (
     <>
       <form action="#">
-        <div className='flex justify-between items-center bg-veryDarkSaturatedBlue w-full pt-3 pb-3.5 pl-16 pr-8 rounded-md text-extraLightGray text-2xl shadow-2xl'>
-          <input type="text" placeholder='Create a new todo...' className='placeholder:text-lightGrayishBlue placeholder:text-xl placeholder:font-normal placeholder:tracking-normal bg-transparent outline-none select-none' value={text} onChange={handleText} />
-          <button type='submit' className='opacity-80 hover:opacity-100 p-2 rounded-lg transition-all' onClick={handleSubmit}><AiOutlinePlusCircle fontSize={"2.1rem"}/></button>
+        <div className='flex justify-between items-center bg-veryDarkSaturatedBlue w-full md:pt-3 md:pb-3.5 md:pl-16 md:pr-8 pt-5 pb-6 pl-10 pr-4 rounded-md text-extraLightGray md:text-2xl text-xl shadow-2xl'>
+          <input type="text" placeholder='Create a new todo...' className='placeholder:text-lightGrayishBlue md:placeholder:text-xl placeholder:text-lg placeholder:font-normal placeholder:tracking-normal bg-transparent outline-none select-none' value={text} onChange={handleText} maxLength={"45"} />
+          <button type='submit' className='hidden md:inline opacity-80 hover:opacity-100 p-2 rounded-lg transition-all' onClick={handleSubmit}><AiOutlinePlusCircle fontSize={"2.1rem"}/></button>
         </div>
       </form>
     </>
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -13,15 +13,18 @@ const Todo = ({ completed, text, id, todoListCheck, todoListDelete }) => {
   }
 
   return (
-    <li className='py-6 px-9 border-b-2 border-veryDarkGrayishBlue flex justify-between items-center'>
-      <div className='flex items-center gap-10'>
-        {completed ? <div className='h-7 w-7 flex items-center rounded-full justify-center ring-2 ring-veryDarkGrayishBlue cursor-pointer bg-gradient-to-br from-cyan-600 via-cyan-600 to-pink-600' onClick={handleCheck}>
-          <img src={checkmark} alt="" className='h-3 select-none'/>
-        </div> : <div className='h-7 w-7 flex items-center rounded-full justify-center ring-2 ring-veryDarkGrayishBlue cursor-pointer' onClick={handleCheck}>
-        </div>}
-        {completed ? <span className='text-2xl text-gray-500 line-through select-none'>{text}</span> : <span className='text-2xl text-lightGrayishBlue select-none'>{text}</span>}
+    <li className='py-6 md:px-9 px-7 border-b-2 border-veryDarkGrayishBlue'>
+      <div className='flex items-center justify-between w-full gap-10'>
+        <div className='flex gap-8 items-center justify-center'>
+          {completed ? <div className='h-7 min-w-[1.75rem] flex items-center rounded-full justify-center ring-2 ring-veryDarkGrayishBlue cursor-pointer bg-gradient-to-br from-cyan-600 via-cyan-600 to-pink-600' onClick={handleCheck}>
+            <img src={checkmark} alt="" className='h-3 select-none'/>
+          </div> : <div className='h-7 min-w-[1.75rem] flex items-center rounded-full justify-center ring-2 ring-veryDarkGrayishBlue cursor-pointer' onClick={handleCheck}>
+          </div>}
+          {completed ? <span className='md:text-2xl text-xl text-gray-500 line-through select-none w-auto'>{text}</span> : <span className='md:text-2xl text-xl text-lightGrayishBlue select-none'>{text}</span>}
+        </div>
+        <button onClick={handleDelete}><img src={cross} alt="" className='h-5 min-w-[1.25rem] hover:invert transition-all select-none'/></button>
       </div>
-      <button onClick={handleDelete}><img src={cross} alt="" className='hover:invert transition-all select-none'/></button>
+      
     </li>
   )
 }
